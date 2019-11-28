ea84bc23a8fe9b41828752ed546b7f3a0f414928
Tim Hütz <tim@huetz.biz>
2019-11-28 12:03:27 +0100

    chore: add basic logging and cli configuration

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,34 @@
+use chrono::Local;
+use clap::{crate_authors, crate_description, crate_name, crate_version};
+use clap::{load_yaml, App};
+use log::LevelFilter;
+
+fn setup_logger() {
+    let _ = fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(LevelFilter::Trace)
+        .chain(std::io::stdout())
+        .apply();
+}
+
 fn main() {
-    println!("Hello, world!");
+    // configure the command line parser
+    let cli_parser_config = load_yaml!("cli.yml");
+    let matches = App::from_yaml(cli_parser_config)
+        .author(crate_authors!())
+        .version(crate_version!())
+        .name(crate_name!())
+        .about(crate_description!())
+        .get_matches();
+
+    // initialize the logger
+    setup_logger();
 }
